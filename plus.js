4d8f72430b1bd7cab89ebf3a159a38e3f950b1f1
oseunghun <102003079+oseunghun@users.noreply.github.com>
2022-09-05 11:36:26 +0900

    9월5일 123교시
    9월5일자바스크립트

---
+++ plus.js
@@ -0,0 +1,20 @@
+function plus(num1, num2){
+    let result;
+    result=num1+num2;
+    return result
+}
+function subtract(num1, num2){
+    let result;
+    result=num1-num2;
+    return result
+}
+function multiply(num1, num2){
+    let result;
+    result=num1*num2;
+    return result
+} 
+function division(num1,num2){
+    let result;
+    result=num1/num2;
+    return result
+}
